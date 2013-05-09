8d2957421fb070654d2c59eb29906fd4cbbc2238
Santiago Lombardi <santiago.lombardi@gmail.com>
2013-05-09 14:24:46 -0300

    cambios en index.js se agrego app

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// Put your custom code here
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+        app.receivedEvent('serverName');
+		
+
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+		$("#" + parentElement).html("Server: ");
+		$.urlParam = function(name){
+		  var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(top.window.location.href); 
+		  return (results !== null) ? results[1] : 0;
+		}
+
+		$("#" + parentElement).html("Servidor: " + $.urlParam("server"));
+		
+    }
+};
+
